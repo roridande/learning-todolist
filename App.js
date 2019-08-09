ac372fd4f11bd7fcb9c6b89f7a53086cabcd323c
William Roridande Aphrodita <31577462+roridande@users.noreply.github.com>
2019-08-09 20:07:16 +0700

    add file

---
+++ App.js
@@ -0,0 +1,86 @@
+import React,  { Component } from 'react';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state={
+      newItem :"",
+      list:[]
+    }
+  }
+
+updateInput(key, value){
+  //update react state
+
+  this.setState({
+    [key] : value
+  })
+}
+
+addItem(){
+  //create item with unique id
+  const newItem={
+    id: 1 + Math.random(),
+    value : this.state.newItem.slice()
+  };
+
+  //copy of current list of items
+  const list = [...this.state.list];
+
+  //add new item to list
+  list.push(newItem);
+
+  //update state with new list and reset newItem input
+  this.setState({
+    list,
+    newItem:""
+  });
+}
+
+deleteItem(id){
+  //copy current list of items
+  const list = [...this.state.list];
+
+  //filter out item being delete
+  const updateList = list.filter(item => item.id !== id);
+
+  this.setState({list: updateList});
+}
+
+render() {
+  return (
+    <div className = "App">
+      Add Todolist item ...
+      <br/>
+      <input
+        type = "text"
+        placeholder = "Type todolist here..."
+        value = {this.state.newItem}
+        onChange={e=>this.updateInput("newItem",e.target.value)}
+      />
+      <button
+        onClick={() => this.addItem()}
+      >
+        Add
+      </button>
+      <br/>
+      <ul> 
+        {this.state.list.map(item => {
+          return(
+          <li key ={item.id}>
+            {item.value}
+            <button
+              onClick = {() => this.deleteItem(item.id)}
+            >
+              X
+            </button>
+          </li>
+          )
+        })}
+      </ul>
+    </div> 
+  )
+}
+}
+export default App;
